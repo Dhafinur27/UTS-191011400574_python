13ec9c3fd9da68fe8fbcdf2a160589118ce19fad
Dhafinur27 <maulanaandre2701@gmail.com>
2022-04-22 09:24:05 +0700

    kecerdasan buatan

---
+++ aldohasiolanpane.py
@@ -0,0 +1,15 @@
+aldohasiolan.py
+#Menginput Angka
+angka = float(input("Tulis Sebuah Angka: "))
+ 
+#Menampilkan Kondisi Angka Positif
+if angka > 0:
+   print("Angka Positif")
+ 
+#Menampilkan Kondisi Angka Nol   
+elif angka == 0:
+   print("Angka Nol")
+ 
+#Menampilkan Kondisi Angka Negatif
+else:
+   print("Angka Negatif")
